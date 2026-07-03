6120a2a518c7d400ae9e9756d3cc0ae4993d1193
suscersal <suscersal@gmail.com>
2026-07-03 15:57:36 +0300

    clear .lua files

--- content/avatar/unification/v2/UnificationClientScript.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local AvatarUnificationFolder = ReplicatedStorage:WaitForChild("AvatarUnification")
-local AdaptCharacterPropertiesAndPhysics =
-	require(AvatarUnificationFolder:WaitForChild("AdaptCharacterPropertiesAndPhysics"))
-local character = script.Parent
-repeat
-	task.wait()
-until character:IsDescendantOf(workspace)
-AdaptCharacterPropertiesAndPhysics.adapt(character, AdaptCharacterPropertiesAndPhysics.ADAPTER_MODES.ALL)
